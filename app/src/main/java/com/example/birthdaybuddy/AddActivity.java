bff28c5ba05f9d83979cc56edaa651a00394fb7c
sudeepanv <vsudeepan@gmail.com>
2025-02-24 18:17:06 +0530

    MainActivity

--- app/src/main/java/com/example/birthdaybuddy/AddActivity.java
+++ app/src/main/java/com/example/birthdaybuddy/AddActivity.java
@@ -1,6 +1,5 @@
 package com.example.birthdaybuddy;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab;
+public class AddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> {
-            Intent intent = new Intent(this,AddActivity.class);
-            startActivity(intent);
-            finish();
         });
     }
 }
